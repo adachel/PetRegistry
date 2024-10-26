3aa90217b32b2732331cd70f29ef5c71ad09b88b
adachel <adachel74@gmail.com>
2024-10-26 18:45:13 +0500

    add menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 import org.example.Menu.Menu;
 
+import javax.xml.crypto.Data;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
